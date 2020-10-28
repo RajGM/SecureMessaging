78456fa73fcd57d3bb8cbced1702430e4e1de4b4
RajGM <rajgmsocial19@gmail.com>
2020-10-29 00:39:06 +0530

    tried multiple things but unable to fetch data in sync

--- app/router/dump.js
+++ app/router/dump.js
@@ -4,6 +4,8 @@ var path = require('path');
 const regProfile = require('./../models/profile');
 const chatW = require('./../models/chatWindow');
 const { MongoClient } = require('mongodb');
+const { resolve } = require('path');
+const { rejects } = require('assert');
 
 var collectionArr;
 
@@ -11,12 +13,12 @@ router.get('/', function (req, res) {
     res.sendFile(path.join(__dirname + './../views/' + 'dump.html'));
 });
 
-router.get('/data',  async function (req, res) {
-    
-    const profile = await getCollectionNames('test');
+router.get('/data', async function (req, res) {
+
+    //const profile = await getCollectionNames('test');
     //handle the null cases here
     const profileChat = await getCollectionData('testtest2');
-    console.log("ConsoleLog from res:"+profileChat);
+    console.log("ConsoleLog from res:" + profileChat);
     res.status(200).json(profile);
 });
 
@@ -38,8 +40,8 @@ function dumpData(DBname) {
 
 async function getCollectionNames(userName) {
     console.log("Start of getCollectionNames");
-    const profile = await regProfile.findOne({ userName:userName });
-    console.log('full profile promise: ' ,profile);
+    const profile = await regProfile.findOne({ userName: userName });
+    console.log('full profile promise: ', profile);
     //handdle the case of profile not found here
     console.log("End of getCollectionNames");
     return profile.chatWindow;
@@ -50,42 +52,43 @@ async function getCollectionData(collName) {
     let MongoClient = require('mongodb').MongoClient;
     const configFile = require('./../../myUrl');
     const url = configFile.mongoURL + configFile.userName + ":" + configFile.password + configFile.restUrl;
-    
-    console.log("Before calling MonogClient connect");
-    let returnCollData = await MongoClient.connect(url,function(err,db){
-        console.log("Inside MongoClient 1");
+
+    MongoClient.connect(url, async (err, client) => {
+
         if (err) throw err;
+        const db = client.db('testdb');
+        console.log("Catch point 1");
+        let dbo = db.collection(collName);
+        console.log("Catch point 2");
+        let pro = new Promise((resolve, rejects) = {
 
-        let dbo =  db.db("testdb");
-        let chatList =  dbo.collection(collName);
-        console.log("Inside MongoClient 2");
-        let test123 =  chatList.find( {}, {projection:{"_id":0}})
-        .toArray( async (err,result)=>{
-            console.log("Inside MongoClient 3");
-            if (err) throw err;
-            console.log("Inside MongoClient 4");
-            await console.log(result);
-            await console.log("test123:"+test123);
         })
+        let fin = await dbo.find({}, { projection: { "_id": 0 } })
+            .toArray(await function (err, data) {
+                if (err) throw err;
+                console.log(data);
+            })
+        console.log("fin", fin);
 
-        
-        //.toArray();
-        console.log("chatList");
-        
-        db.close();
-    });
-    
-    
-    console.log("End of getCollectionData");
-    return returnCollData;
-};
-getCollectionData("testtest2");
+        console.log("End of getCollectionData");
+    })
+
+    return;
+}
+
+async function callME() {
+    let ok = await getCollectionData("testtest2");
+    //console.log("Typeof OK:", typeof ok);
+    //console.log("OK:", ok);
+}
+
+callME();
 
 async function getWholeChat(userName) {
     console.log("Before calling getCollectionNames");
     var newArr = await getCollectionNames(userName);
     collectionArr = newArr;
-    console.log("Testing local return:"+newArr);
+    console.log("Testing local return:" + newArr);
     console.log("After calling getCollectionNames");
 }
 
