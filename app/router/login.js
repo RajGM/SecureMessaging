53d467eff637d0762a5ca1302eefb14617e527a1
RajGM <rajgmsocial19@gmail.com>
2020-10-31 16:56:49 +0530

    loginHelper and login serverside exported and done

--- app/router/login.js
+++ app/router/login.js
@@ -25,6 +25,7 @@ router.post('/', async (req,res)=>{
         userName:"",
         authToken:""
     }
+    
     if(loginState=="correct"){
         responseObj.logInfo="Success";
         responseObj.userName=profileValues.username;
--- app/serverSideJs/loginHelper.js
+++ app/serverSideJs/loginHelper.js
@@ -2,7 +2,23 @@ const logProfile = require('../models/profile');
 const authToken = require('../models/authToken');
 const configFile = require('./../../myUrl');
 
-async function loginProfile(userName,password) {
+async function loginProfile(userName, password) {
+
+    //verify then update
+    let logA = await verifyUNamePass(userName, password);
+    if (logA == "correct") {
+        authTok = await updateAuthToken(userName);
+        console.log("authTok return State:", authTok);
+        console.log("updated");
+        return "correct";
+    } else {
+        console.log("incorrect");
+        return "incorrect";
+    }
+
+}
+
+async function verifyUNamePass(userName, password) {
     let MongoClient = require('mongodb').MongoClient;
     const url = configFile.mongoURL + configFile.userName + ":" + configFile.password + configFile.restUrl;
     let dataArr;
@@ -10,11 +26,11 @@ async function loginProfile(userName,password) {
         .catch(err => console.log(err));
 
     console.log("Values in loginHelper function");
-    console.log("userName:"+userName+" password:"+password);
+    console.log("userName:" + userName + " password:" + password);
     try {
         const db = client.db('testdb').collection("userprofiles");
-        dataArr = await db.find({ userName }, { projection: { "_id": 0 ,"__v":0} })
-            .toArray();        
+        dataArr = await db.find({ userName }, { projection: { "_id": 0, "__v": 0 } })
+            .toArray();
     }
     catch (err) {
         console.log(err);
@@ -23,26 +39,71 @@ async function loginProfile(userName,password) {
     }
 
     console.log(typeof dataArr);
-    console.log("dataArr",dataArr);
+    console.log("dataArr", dataArr);
     if (Object.keys(dataArr).length === 0) {
         console.log("blank");
         return "notExists"
-    }else{
-        if(password==dataArr[0].password){
+    } else {
+        if (password == dataArr[0].password) {
             return "correct";
-        }else{
+        } else {
             return "incorrect";
         }
     }
 
 }
 
-async function callME(){
+async function updateAuthToken(userName) {
+    let MongoClient = require('mongodb').MongoClient;
+    const url = configFile.mongoURL + configFile.userName + ":" + configFile.password + configFile.restUrl;
+    let dataArr;
+    let doc;
+    let client = await MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+        .catch(err => console.log(err));
+
+    try {
+        client = await MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+            .catch(err => console.log(err));
+
+        const db = client.db('testdb').collection('authtokens');
+        dataArr = await db.updateOne({ userName: userName }, { $set: { authToken: "ABCDEFG", authExpire: "Soon2Expire" } }, { upsert: true, useFindAndModify: false });
+            /*
+            .then(updatedDoc => {
+                if (updatedDoc) {
+                    doc = updatedDoc;
+                    console.log("updatedDoc Fail:" + updatedDoc);
+                } else {
+                    console.log("updatedDoc:" + updatedDoc);
+                }
+            })
+            .catch(err => {
+                console.log(err);
+            })
+            */
+        console.log("authTOken update state:" + dataArr);
+    } catch (err) {
+        console.log("authToken updation error:" + err);
+    } finally {
+        client.close();
+    }
+
+    return "ABCDEFGH";
+}
+
+async function callME() {
     let userName = "test";
     let password = "123";
-    let logA = await loginProfile(userName,password);
-    console.log("type of logA:"+ typeof logA);
+    let authTok = "";
+    let logA = await verifyUNamePass(userName, password);
+    console.log("type of logA:" + typeof logA);
     console.log(logA);
+    if (logA == "correct") {
+        authTok = await updateAuthToken(userName);
+        console.log("authTok return State:", authTok);
+        console.log("updated");
+    } else {
+        console.log("incorrect");
+    }
 }
 
 //callME();
