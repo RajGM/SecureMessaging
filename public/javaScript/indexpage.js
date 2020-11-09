2df62cadf80b2a26869fe8a2d0bb9d4c49e69a15
RajGM <rajgmsocial19@gmail.com>
2020-11-09 09:40:11 +0530

    index refer working

---
+++ public/javaScript/indexpage.js
@@ -0,0 +1,9 @@
+console.log("This is index page");
+
+function loginPage(){
+    window.location.href = "http://localhost:3000/login"
+}
+
+function registerPage(){
+    window.location.href = "http://localhost:3000/register"
+}
