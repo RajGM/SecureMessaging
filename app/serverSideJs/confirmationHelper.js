6ddb3ff5bad731d2bac2a4c878b76153c67a70cf
RajGM <rajgmsocial19@gmail.com>
2021-05-24 00:13:24 +0530

    confirmation.js and confirmationHelper.js are optimized now using allHelper.js

--- app/serverSideJs/confirmationHelper.js
+++ app/serverSideJs/confirmationHelper.js
@@ -1,20 +1,14 @@
 //update email verification status of user
-async function updateEmailVerificationStatus(email) {
-    let MongoClient = require('mongodb').MongoClient;
-    const url = process.env.mongoURL + process.env.mongoUserName + ":" + process.env.mongoPassword + process.env.mongoRestUrl;
+async function updateEmailVerificationStatus(email,mongoClient) {
     let dataArr;
-    let client = await MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
-        .catch(err => console.log(err));
-
+    
     try {
-        const db = client.db(process.env.mongoDBName).collection('userprofiles');
+        const db = mongoClient.db(process.env.mongoDBName).collection('userprofiles');
         dataArr = await db.updateOne({ email: email }, { $set: { isEmailVerified:true } }, { upsert: true, useFindAndModify: false });
 
     } catch (err) {
         console.log("authToken updation error:" + err);
-    } finally {
-        client.close();
-    }
+    } 
 
     return "ABCDEFGH";
 }
